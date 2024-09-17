6d4804743ae655bacf952727e273e1879680442f
Sahil <leviack629@gmail.com>
2024-09-17 15:44:55 +0530

    screenshot using puppeteer

---
+++ index.js
@@ -0,0 +1,14 @@
+import puppeteer from "puppeteer";
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.npmjs.com/package/puppeteer");
+
+  await page.screenshot({ path: `${Math.random()}.png` });
+
+  await browser.close();
+}
+
+main();
